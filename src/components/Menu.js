5a3bd95ba5bcc6871d9b7cb78632913fade47c5c
recoilmatt <recoilmatt@gmail.com>
2020-05-08 09:20:32 -0700

    Revert "Revert "new update 05.7""
    This reverts commit 2cb250b0dbe9c4eee3b40bc6c06b81d274158c4a.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,14 +6,14 @@ const Menu = (props) => (
     <nav id="menu">
         <div className="inner">
             <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="# ">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="# ">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="# ">Generic</Link></li>
-                
+                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
             </ul>
             <ul className="actions vertical">
-                <li><a href="# " className="button special fit">Get Started</a></li>
-                <li><a href="# " className="button fit">Log In</a></li>
+                <li><a href="#" className="button special fit">Get Started</a></li>
+                <li><a href="#" className="button fit">Log In</a></li>
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
