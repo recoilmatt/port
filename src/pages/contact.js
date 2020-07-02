f8f6ac00a77d378cf5589d364c408360450811a3
recoilmatt <recoilmatt@gmail.com>
2020-07-01 18:14:06 -0700

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import matt from '../assets/images/mattmatt.jpg'
+import pf from '../assets/images/pf.jpg'
 
 const Generic = props => (
   <Layout>
@@ -20,21 +20,15 @@ const Generic = props => (
             <div className="col-6"><p>By integrating my experiences in biological sciences, healthcare, and digital marketing, I am able to apply my skills towards the design process in order to create meaningful digital products that solve problems and improve usability. I'd like to expand my skills to become a UX Engineer in the future.
             </p>
             <p>My hobbies include photography, travel, and matcha related things. I'm cooler on Instagram <a href="http://www.instagram.com/recoilmatt">@recoilmatt</a></p>
-            {/*
-            <div className="col-4">
-            <img src={Contact} style={{margin:"auto",borderRadius: "8px", maxWidth: "100%", maxHeight: "20vh"}}alt="" />
-            <img src={Contact} style={{margin:"auto",borderRadius: "8px", maxWidth: "100%", maxHeight: "20vh"}}alt="" />
-            <img src={Contact} style={{margin:"auto",borderRadius: "8px", maxWidth: "100%", maxHeight: "20vh"}}alt="" />
+       
 
-            </div>
-              */}
             </div>
 
           
+           <div className="col-6"> 
+           <img src={pf} style={{display:"block",margin:"auto", borderRadius: "8px", maxWidth: "100%", maxHeight: "50vh"}} alt="" />
+          </div>
 
-            <div className="col-6"> 
-           <img src={matt} style={{display:"block",margin:"auto",borderRadius: "8px", maxWidth: "100%", maxHeight: "50vh"}}alt="" />
-            </div>
           </div>
         </div>
     </section>
