34a48f8abf316e447bcb0c000a768feb85c0e6e8
recoilmatt <recoilmatt@gmail.com>
2020-10-27 18:00:03 -0700

    arc addition and misc

--- src/pages/misc.js
+++ src/pages/misc.js
@@ -3,9 +3,12 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import anime from '../assets/images/anime.png'
 import shef from '../assets/images/shefmock.png'
-import TravelFinal from '../assets/images/TravelFinal.jpg'
+import travelfinal from '../assets/images/TravelFinal.jpg'
 import health from '../assets/images/health.webm'
+import arc from '../assets/images/arc.gif'
 import Tabs from '../components/Tabs'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 const Projects = props => (
   <Layout>
@@ -22,21 +25,34 @@ const Projects = props => (
           </header>
           <div className="grid-wrapper">
             <div className="col-6">
-              <video width="100%" height="100%" autoPlay muted loop>
-                <source src={health} type="video/webm" />
-              </video>
+              <Zoom>
+                <span className="image main">
+                  <img src={arc} alt="Arc" />
+                </span>
+              </Zoom>
             </div>
             <div className="col-6">
-              <span className="image main">
-                <img src={TravelFinal} alt="Travel Final" />
-              </span>
+              <Zoom>
+                <span className="image main">
+                  <img src={travelfinal} alt="Travel Final" />
+                </span>
+              </Zoom>
             </div>
           </div>
           <div className="grid-wrapper">
             <div className="col-6">
-              <span className="image main">
-                <img src={anime} alt="anime watchlist" />
-              </span>
+              <Zoom>
+                <span className="image main">
+                  <img
+                    src={anime}
+                    style={{
+                      maxHeight: '300px',
+                      objectFit: 'cover',
+                    }}
+                    alt="anime watchlist"
+                  />
+                </span>
+              </Zoom>
               <section class="overview">
                 <div
                   className="col-12"
@@ -57,9 +73,11 @@ const Projects = props => (
             </div>
 
             <div className="col-6">
-              <span className="image main">
-                <img src={shef} alt="shef" />
-              </span>
+              <Zoom>
+                <span className="image main">
+                  <img src={shef} alt="shef" />
+                </span>
+              </Zoom>
               <section class="overview">
                 <div
                   className="col-12"
