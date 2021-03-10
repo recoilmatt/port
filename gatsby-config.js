95080591db634f3124e954d3f569b1cce69c5e06
recoilmatt <recoilmatt@gmail.com>
2021-03-10 00:12:51 -0800

    1212

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,28 +4,45 @@ module.exports = {
     author: 'Matt Lai',
     description: 'UX Design',
   },
-
   plugins: [
+    '',
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: [
-          'G-BHZHN0WQ9F', // Google Analytics / GA
-        ],
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ['G-BHZHN0WQ9F'],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
         gtagConfig: {
           optimize_id: 'OPT_CONTAINER_ID',
           anonymize_ip: true,
           cookie_expires: 0,
         },
-
+        // This object is used for configuration specific to this plugin
         pluginConfig: {
+          // Puts tracking script in the head instead of the body
           head: false,
-
+          // Setting this parameter is also optional
           respectDNT: true,
-
+          // Avoids sending pageview hits from custom paths
           exclude: ['/preview/**', '/do-not-track/me/too/'],
         },
       },
     },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
   ],
 }
