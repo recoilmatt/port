e0fca2e11efce9e1709c70055ac6125c1945b772
recoilmatt <recoilmatt@gmail.com>
2021-03-10 11:49:12 -0800

    added scrollbars

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,51 +4,66 @@ import '../assets/scss/main.scss'
 import Header from './Header'
 import Menu from './Menu'
 
+//Scrollbars
+import { Scrollbars } from 'react-custom-scrollbars'
+
 import Footer from './Footer'
 
 class Layout extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isMenuVisible: false,
-            loading: 'is-loading'
-        }
-        this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMenuVisible: false,
+      loading: 'is-loading',
     }
+    this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  }
 
-    componentDidMount () {
-        this.timeoutId = setTimeout(() => {
-            this.setState({loading: ''});
-        }, 100);
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({ loading: '' })
+    }, 100)
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
     }
+  }
 
-    componentWillUnmount () {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-    }
+  handleToggleMenu() {
+    this.setState({
+      isMenuVisible: !this.state.isMenuVisible,
+    })
+  }
 
-    handleToggleMenu() {
-        this.setState({
-            isMenuVisible: !this.state.isMenuVisible
-        })
-    }
+  render() {
+    const { children } = this.props
 
-    render() {
-        const { children } = this.props
+    return (
+      <div
+        className={`body ${this.state.loading} ${
+          this.state.isMenuVisible ? 'is-menu-visible' : ''
+        }`}
+      >
+        {' '}
+        <Scrollbars
+          autoHide
+          autoHideTimeout={1000}
+          autoHideDuration={200}
+          style={{ width: '100vw', height: '100vh' }}
+        >
+          <div id="wrapper">
+            <Header onToggleMenu={this.handleToggleMenu} />
+            {children}
 
-        return (
-            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-                <div id="wrapper">
-                    <Header onToggleMenu={this.handleToggleMenu} />
-                    {children}
-                    
-                    <Footer />
-                </div>
-                <Menu onToggleMenu={this.handleToggleMenu} />
-            </div>
-        )
-    }
+            <Footer />
+          </div>
+        </Scrollbars>
+        <Menu onToggleMenu={this.handleToggleMenu} />
+      </div>
+    )
+  }
 }
 
 export default Layout
