d00b328338ac834c6a0b4529277291de020b5f0b
recoilmatt <recoilmatt@gmail.com>
2021-04-17 22:52:50 -0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description: 'UX Design',
   },
   plugins: [
+    'gatsby-plugin-open-graph-images',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
